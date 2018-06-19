d05feceaf2b79d4f0299464a378aa04d37411371
Jenna <murrelljenna@gmail.com>
2018-06-19 15:34:21 -0400

    Changed certain spacing habits to be more consistent. No more empty lines after open curly brackets.

--- gamelibrary.js
+++ gamelibrary.js
@@ -1,28 +1,18 @@
 
-function pickRange(start, end){
-
+function pickRange (start, end) {
 	return Math.floor(Math.random() * ((end - start + 1)) + start)
-
 }
 
-function isInRange(needle, min, max){
-
-	if ((needle >= min) && (needle <= max)){
-	
+function isInRange (needle, min, max) {
+	if ((needle >= min) && (needle <= max)){	
 		return true
-	
 	}else{
-	
 		return false
-	
 	}
-
 }
 
-function range(startAt, size) {
-
+function range (startAt, size) {
 	return [...Array(size).keys()].map(i => i + startAt);
-
 }
 
 /*
@@ -31,32 +21,14 @@ function range(startAt, size) {
 
 */
 
-function MapEntity(x1, y1, width, height){
-	
+function MapEntity (x1, y1, width, height) {
 	this.x1 = x1
 	this.y1 = y1
 	this.width = width
 	this.height = height
-
 }
 
-function Player(x1, y1, width, height){
-	
-	this.x1 = x1
-	this.y1 = y1
-	this.width = width
-	this.height = height
-	this.xSpeed = 0
-	this.ySpeed = 0
-	this.collisionNorth = false
-	this.collisionSouth = false
-	this.collisionEast = false
-	this.collisionWest = false
-
-}
-
-function Monster(x1, y1, width, height){
-	
+function Player (x1, y1, width, height) {
 	this.x1 = x1
 	this.y1 = y1
 	this.width = width
@@ -67,53 +39,29 @@ function Monster(x1, y1, width, height){
 	this.collisionSouth = false
 	this.collisionEast = false
 	this.collisionWest = false
-
 }
 
-
-
-// Pathfinding functions.
-
-function playerDirection(x, y){
-
-	if (y > playerPosY){
-		playerDirectionNorth = true;
-		playerDirectionSouth = false;
-	} else if (y <= playerPosY){
-		playerDirectionSouth = true;
-		playerDirectionNorth = false;
-	}
-
-	if (x > playerPosX){
-		playerDirectionWest = true;
-		playerDirectionEast = false;
-	}else if (x <= playerPosX){
-		playerDirectionEast = true;
-		playerDirectionWest = false;
-	}
-
-}	
-
-function obstacleCheck(x, y){
-	for (var counter in masterColArray){
-		if (
-		masterColArray[counter].x2 < x ||  
-		x < masterColArray[counter].x1 ||
-		masterColArray[counter].y2 < y ||
-		y < masterColArray[counter].y1
-		){
-			return false
-		}else{	
-			return true
-		}
-	}
+function Monster (x1, y1, width, height) {
+	this.x1 = x1
+	this.y1 = y1
+	this.width = width
+	this.height = height
+	this.xSpeed = 0
+	this.ySpeed = 0
+	this.collisionNorth = false
+	this.collisionSouth = false
+	this.collisionEast = false
+	this.collisionWest = false
 }
 
+/*
+
+Pathfinding functions.
+
+*/
 
 function roombaMovement(movingEntity){
-
 	if (movingEntity.collisionDirectionNorth === true && movingEntity.collisionDirectionWest === false && movingEntity.collisionDirectionEast === false){
-		
 		coinFlip = Math.floor(Math.random() * 2);
 		if (coinFlip === 0){
 			movingEntity.xSpeed = 2
@@ -121,30 +69,24 @@ function roombaMovement(movingEntity){
 			movingEntity.xSpeed = 2
 		}
 	}
-
 	if (movingEntity.collisionDirectionSouth === true && movingEntity.collisionDirectionWest === false && movingEntity.collisionDirectionEast === false){
-		
 		coinFlip = Math.floor(Math.random() * 2);
 		if (coinFlip === 0){
 			movingEntity.xSpeed = 2
 		}else{
 			movingEntity.xSpeed = 2
 		}
-	
 	}
-
 	if (movingEntity.collisionDirectionWest === true && movingEntity.collisionDirectionNorth === false && movingEntity.collisionDirectionSouth === false){
-		
 		coinFlip = Math.floor(Math.random() * 2);
 		if (coinFlip === 0){
 			movingEntity.ySpeed = 2
 		}else{
 			movingEntity.ySpeed = 2
 		}
-	}
 
+	}
 	if (movingEntity.collisionDirectionEast === true && movingEntity.collisionDirectionNorth === false && movingEntity.collisionDirectionSouth === false){
-		
 		coinFlip = Math.floor(Math.random() * 2);
 		if (coinFlip === 0){
 			movingEntity.ySpeed = 2
@@ -202,14 +144,12 @@ function collisionDetectDebug(counter){
 }
 
 function detectCollisionSide(needleColArray){
-
 	let northSignal = false
 	let southSignal = false
 	let eastSignal = false
 	let westSignal = false
 
 	for (var counter in masterColArray){
-		
 		if ((isInRange(needleColArray.x1, masterColArray[counter].x1, masterColArray[counter].x2) &&
 		isInRange(needleColArray.y1, (masterColArray[counter].y2 - 1), (masterColArray[counter].y2 + 1))) || 
 		(isInRange(masterColArray[counter].x2, needleColArray.x1, needleColArray.x2) &&
@@ -219,7 +159,6 @@ function detectCollisionSide(needleColArray){
 				needleColArray.ySpeed = 0
 			}
 		}
-
 		if ((isInRange(needleColArray.x2, masterColArray[counter].x1, masterColArray[counter].x2) &&
 		isInRange(needleColArray.y2, (masterColArray[counter].y1 - 1), (masterColArray[counter].y1 + 1))) ||
 		(isInRange(masterColArray[counter].x1, needleColArray.x1, needleColArray.x2) &&
@@ -229,7 +168,6 @@ function detectCollisionSide(needleColArray){
 				needleColArray.ySpeed = 0
 			}
 		}
-
 		if ((isInRange(needleColArray.y1, masterColArray[counter].y1, masterColArray[counter].y2) &&
 		isInRange(needleColArray.x1, (masterColArray[counter].x2 - 1), (masterColArray[counter].x2 + 1))) ||
 		(isInRange(masterColArray[counter].y2, needleColArray.y1, needleColArray.y2) &&
@@ -239,7 +177,6 @@ function detectCollisionSide(needleColArray){
 				needleColArray.xSpeed = 0
 			}
 		}
-
 		if ((isInRange(needleColArray.y2, masterColArray[counter].y1, masterColArray[counter].y2) &&
 		isInRange(needleColArray.x2, (masterColArray[counter].x1 - 1), (masterColArray[counter].x1 + 1)))||
 		(isInRange(masterColArray[counter].y1, needleColArray.y1, needleColArray.y2) &&
@@ -255,22 +192,18 @@ needleColArray.collisionDirectionNorth = northSignal;
 needleColArray.collisionDirectionSouth = southSignal;
 needleColArray.collisionDirectionEast = eastSignal;
 needleColArray.collisionDirectionWest = westSignal;
-	
 }
 
 function masterColArrayDebug(){
-	
 	ctx.strokeStyle="#FF0000";
 	for (index in masterColArray){
 		ctx.moveTo((masterColArray[index].x1), (masterColArray[index].y1))
 		ctx.lineTo((masterColArray[index].x2), (masterColArray[index].y2))
 		ctx.stroke()
 	}
-
 }
 
 function updatePosition(movingEntity){
-
 	xClear = movingEntity.x1
 	yClear = movingEntity.y1
 
@@ -284,24 +217,25 @@ function updatePosition(movingEntity){
 	
 	ctx2.clearRect(xClear, yClear, movingEntity.width, movingEntity.height);
 	ctx2.fillRect(movingEntity.x1, movingEntity.y1, movingEntity.width, movingEntity.height);
-
-
-
 }
+
+/*
+
+	Map Object Generation
+
+*/
+
 
 // Canvas Drawing Functions
 
 function drawLines(){
-
 	for (counter = 0; counter < 14;){
-
 		xCoord = pickRange(0, canvasWidth);
 		yCoord = pickRange(0, canvasHeight);
 
 		ctx.beginPath();
 		ctx.moveTo(xCoord, yCoord);
 		while (pickRange(0, 1) === 0){	
-
 			x1 = xCoord;
 			y1 = yCoord;
 
